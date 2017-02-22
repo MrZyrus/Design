6c4db3ae4f6ca815eb436583ccc20cf94710b82b
MrZyrus <kaiser705@gmail.com>
2017-02-22 16:28:15 -0400

    More stuff now

--- Design.cpp
+++ Design.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <string>
 #include <fstream> 
-
+#include <queue>
 
 using namespace std;
 
@@ -18,32 +18,46 @@ struct Edges {
 
 struct Graph {
 	struct Edges *edges;
+	int vertex_counter;
+	int edge_counter;
+	int required_edges;
+	int non_required_edges;
 };
 
+int djisktra(int origin, int destination, Graph graph) {
+
+};
 
 int main(int argc, char* argv[]) {
-	string edge_string;
+	struct Graph graph;
 	ifstream file (argv[1]);
+	string edge_string;
 	string subString;
 	int i = 0; //iterator
 
-	getline(file, edge_string);  //Number of vertices is not necessary
-	getline(file, edge_string);  //For getting the number of edges
+	getline(file, edge_string);
 	istringstream it_sub_string(edge_string);
 
 	while(it_sub_string) {
 		it_sub_string >> subString;
 	}
+	graph.vertex_counter = stoi(subString);
 
-	int ed = stoi(subString); //Get the number of edges
-	struct Graph graph;
-	graph.edges = new Edges[ed];
+	getline(file, edge_string);  //For getting the number of edges
+	it_sub_string.str(edge_string);
+	it_sub_string.clear();
 
-	while (getline(file,edge_string)){
-		if (isdigit(edge_string[0])){
-				cout << edge_string;
-				printf("\n");
-				istringstream it_sub_string(edge_string);
+	while(it_sub_string) {
+		it_sub_string >> subString;
+	}
+	graph.required_edges = stoi(subString);
+	graph.edge_counter = graph.required_edges;
+	graph.edges = new Edges[graph.edge_counter];
+
+ 	while (getline(file, edge_string)) {
+		it_sub_string.str(edge_string);
+		it_sub_string.clear();
+		if (isdigit(edge_string[0])) {
 				it_sub_string >> subString;
 				graph.edges[i].origin = stoi(subString);
 				it_sub_string >> subString;
@@ -53,6 +67,29 @@ int main(int argc, char* argv[]) {
 				it_sub_string >> subString;
 				graph.edges[i].benefit = stoi(subString);
 				i++;
-		}	
+		}
+
+		else {
+			while(it_sub_string) {
+				it_sub_string >> subString;
+			}
+			graph.non_required_edges = stoi(subString);
+			graph.edge_counter += graph.non_required_edges;
+			graph.edges = (Edges*)realloc(graph.edges, sizeof(Edges) * (graph.edge_counter));
+		}
 	}
+
+	 required_nodes = new int[graph.required_edges*2];
+	i = 0;
+	while(i < graph.required_edges) {
+		required_nodes[i] = graph.edges[i].origin;
+		required_nodes
+	}
+
+	i = 0;
+	while(i < graph.edge_counter){
+		cout << i << ' ';
+		cout << graph.edges[i].origin << ' ' << graph.edges[i].destination << ' ' << graph.edges[i].cost << ' ' << graph.edges[i].benefit << '\n';		
+		i++;
+	}	
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+main:		Design.cpp
+		g++ -std=c++11 -o Design Design.cpp
